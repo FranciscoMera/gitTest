0f67ca8594f3b132a98a588b2997245a78c02db2
franciscomera <fcomera.ipn@gmail.com>
2017-06-24 14:00:23 -0500

    Modulos para compilar en CLION

---
+++ Prueba/cmake-build-debug/CMakeFiles/Prueba.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prueba.dir/Prueba.c.o"
+  "Prueba.pdb"
+  "Prueba"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Prueba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
